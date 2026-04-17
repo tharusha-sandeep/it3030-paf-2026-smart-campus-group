9c934715a89c145d07d7bb52df88b78b4eed4d55
Tharusha <tharusandeep2001@gmail.com>
2026-04-17 13:21:27 +0530

    Added Spring Boot backend

---
+++ backend/smart-campus-api/src/test/java/com/smartcampus/smart_campus_api/SmartCampusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcampus.smart_campus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
